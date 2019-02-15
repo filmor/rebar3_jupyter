281e62e73009254ac9af4efedb1b2bd2081eba1c
Benedikt Reinartz <filmor@gmail.com>
2019-02-15 07:58:26 +0100

    Current state

--- src/rebar3_jupyter_prv.erl
+++ src/rebar3_jupyter_prv.erl
@@ -1,30 +1,39 @@
 -module(rebar3_jupyter_prv).
 
--export([init/1, do/1, format_error/1]).
+-behaviour(provider).
 
--define(PROVIDER, rebar3_jupyter).
--define(DEPS, [app_discovery]).
+-export(
+   [
+    init/1,
+    do/1,
+    format_error/1
+   ]).
 
-%% ===================================================================
-%% Public API
-%% ===================================================================
+-define(PROVIDER, jupyter).
+-define(DEPS, [compile]).
+
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    Provider = providers:create([
-            {name, ?PROVIDER},            % The 'user friendly' name of the task
-            {module, ?MODULE},            % The module implementation of the task
-            {bare, true},                 % The task can be run by the user, always true
-            {deps, ?DEPS},                % The list of dependencies
-            {example, "rebar3 rebar3_jupyter"}, % How to use the plugin
-            {opts, []},                   % list of options understood by the plugin
-            {short_desc, "A rebar plugin"},
-            {desc, "A rebar plugin"}
-    ]),
-    {ok, rebar_state:add_provider(State, Provider)}.
+    Provider = providers:create(
+                 [
+                  {name, ?PROVIDER},
+                  {module, ?MODULE},
+                  {bare, true},
+                  {deps, ?DEPS},
+                  {example, "rebar3 jupyter"},
+                  {opts, []},
+                  {short_desc, "Run Jupyter kernel like a shell"},
+                  {desc, "Jupyter plugin to start notebooks or console shells"}
+                 ]),
 
+    State1 = rebar_state:add_provider(State, Provider),
+    {ok, State1}.
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
+    code:add_pathsa(rebar_state:code_paths(State, all_deps)),
+
+    ierl_exec:exec(jupyter, JArgs),
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
